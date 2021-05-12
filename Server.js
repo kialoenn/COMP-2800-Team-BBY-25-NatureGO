ab3d8c74cfb7910ec959c92d22cb1c2ba7889166
Neeraj Kumar <b.neerajkumar9666@gmail.com>
2021-05-13 01:32:36 +0530

    Firebase setup in server

--- Server.js
+++ Server.js
@@ -2,8 +2,22 @@
 const express = require('express');
 const fs = require("fs");
 const app = express();
-const path = require('path')
+const path = require('path');
 const PORT = process.env.PORT || 5000
+
+var firebase = require('firebase');
+var firebaseConfig = {
+    apiKey: "AIzaSyAI2XYfwipMKzMn7accv1VmborqKBg9j_E",
+    authDomain: "naturego-e74d6.firebaseapp.com",
+    projectId: "naturego-e74d6",
+    storageBucket: "naturego-e74d6.appspot.com",
+    messagingSenderId: "641465486710",
+    appId: "1:641465486710:web:8bb01d58ea9d7136083e57"
+};
+// Initialize Firebase
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 // GENERAL CONSTANTS
 const msg404 = 'These are not the codes that you are looking for.';
 
