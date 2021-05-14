0d8ce08746cceb89a1916fa92a095fb8d42489d3
Neeraj Kumar <b.neerajkumar9666@gmail.com>
2021-05-15 02:30:36 +0530

    Display the animal on info page based on picture

--- private/js/info.js
+++ private/js/info.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-
     var info = localStorage.getItem("animalinfo");
+
+    $('#info_tabe').prepend('<tr><th>Animal Type</th><td>'+info+'</td></tr>');
+
     console.log(info);
+
 
 });
