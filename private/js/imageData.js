1dcf1365dbba1c7b823d2ae01da07d5c45754d66
syakisoba <syakisoba@gmail.com>
2021-05-19 14:33:34 -0700

    adding map for gps location

--- private/js/imageData.js
+++ private/js/imageData.js
@@ -1,4 +1,7 @@
+document.getElementById("selectImgLocation").style.visibility = "hidden";
+
 document.getElementById("file-input").setAttribute("onchange", "previewFile()");
+window.initMap = initMap;
 
 //preview replace image
 window.previewFile = function previewFile() {
@@ -26,16 +29,29 @@ window.previewFile = function previewFile() {
 let pos;
 function getGPSLatitudeLongitude(imgABC) {
   if (typeof EXIF.getTag(imgABC, "GPSLatitudeRef") !== "undefined") {
+    document.getElementById("selectImgLocation").style.visibility = "hidden";
     pos = {
       lat: convertDMStoLatLong(EXIF.getTag(imgABC, "GPSLatitude")[0], EXIF.getTag(imgABC, "GPSLatitude")[1], EXIF.getTag(imgABC, "GPSLatitude")[2], EXIF.getTag(imgABC, "GPSLatitudeRef")),
       lng: convertDMStoLatLong(EXIF.getTag(imgABC, "GPSLongitude")[0], EXIF.getTag(imgABC, "GPSLongitude")[1], EXIF.getTag(imgABC, "GPSLongitude")[2], EXIF.getTag(imgABC, "GPSLongitudeRef")),
     } 
   }else {
-    pos = {
-      lat: "",
-      lng: ""
-    }
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+        };
+      console.log("Current Location: " + pos);
+    },
+      //geoError()
+      initMap()
+    );
+  } else {
+    //geoError();
+    initMap();
   }
+}
     return pos;
   }
 
@@ -50,6 +66,11 @@ function convertDMStoLatLong(hour, minute, second, position) {
 
 export {pos};
 
+function geoError(){
+  console.log("trying to init map!");
+  document.getElementById("selectImgLocation").style.visibility = "visible";
+}
+
 // Note: SKU: Basic Data Using the fields parameter in your Place Details or
 //  Find Place request, you can limit the response to only those fields specified. 
 //  Fields in the Basic category are included in the base cost of the Places request 
@@ -57,3 +78,38 @@ export {pos};
 //  any of these fields are requested: address_component, adr_address, business_status,
 //   formatted_address, geometry, icon, name, permanently_closed, photo, place_id,
 //    plus_code, type, url, utc_offset, vicinity.
+
+function initMap() {
+  document.getElementById("selectImgLocation").style.visibility = "visible";
+
+  console.log("inside initMap");
+  let posTemp = {
+    lat: 49.25076313248947,
+    lng: -123.0017895306895
+  };
+  const map = new google.maps.Map(document.getElementById("selectImgLocation"), {
+    zoom: 8,
+    center: posTemp,
+  });
+  // Create the initial InfoWindow.
+  let infoWindow = new google.maps.InfoWindow({
+    content: "Click the map to get Lat/Lng!",
+    position: posTemp,
+  });
+  infoWindow.open(map);
+  // Configure the click listener.
+  map.addListener("click", (mapsMouseEvent) => {
+    posTemp = mapsMouseEvent.latLng;
+    // Close the current InfoWindow.
+    infoWindow.close();
+    // Create a new InfoWindow.
+    infoWindow = new google.maps.InfoWindow({
+      position: posTemp,
+    });
+    infoWindow.setContent(
+      JSON.stringify(posTemp.toJSON(), null, 2)
+    );
+    infoWindow.open(map);
+  });
+
+}
