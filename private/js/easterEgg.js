37fa2d5207723a675b1cf919a22d7cf67bfb2229
Man Sun <msun45@my.bcit.ca>
2021-05-21 12:10:19 -0700

    modify easter egg feature

--- private/js/easterEgg.js
+++ private/js/easterEgg.js
@@ -135,10 +135,12 @@ $(document).ready(function() {
         // This image is downloaded from https://dribbble.com/shots/5771570-Bird/attachments/5771570-Bird?mode=media
         if (trigger[0] === 1 && trigger[1] === 3) {
             Swal.fire({
-                width: 1000,
+                //width: 50%,
+                height: 500,
                 imageHeight: 500,
-                imageUrl: '/img/bird-dribbble.gif',
-                imageAlt: 'birdSquirrel',
+                background: 'url(/img/bird-dribbble.gif)',
+                //imageUrl: '/img/bird-dribbble.gif',
+                //imageAlt: 'birdSquirrel',
                 title: 'You find our Easter Egg!!!'
               })
         }
